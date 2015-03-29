9fdb08a80cff8077a19b2ae60a68fa42af914d8c
carso <carso.leong@outlook.com>
2015-03-29 13:53:17 +0800

    cheaplak new project

---
+++ CheaplakFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheaplakFood.Startup))]
+namespace CheaplakFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
